0be0ff09ca60bdd82b71716d84dba4bdb486bfaa
WinnieBlue <50311579+WinnieBlue@users.noreply.github.com>
2019-05-12 18:13:22 +1000

    Update Assignment_3.c
    Added structs and some of the function prototypes.

--- Assignment_3.c
+++ Assignment_3.c
@@ -1,11 +1,78 @@
 // Assignment 3
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int main(){
-  
-  return 0;
-  printf("Test"\n);
+/*Insert structs here*/
+struct payment{
+    int card_number;
+    char cardholder;
+    int expiry;
+    int CVC;
+};
+typedef struct payment payment_t;
+
+struct date{
+    int day;
+    int month;
+    int year;
+    int hour;
+    int minute;
+};
+typedef struct date date_t;
+
+struct user
+    char first_name;
+    char last_name;
+    int birthdate;
+    int ID;
+    char password;
+    payment_t payment;
+};
+typedef struct user user_t;
+
+struct transaction{
+    payment_t payment;
+    date_t date;
+
+}
+
+
+
+
+/*Insert function protoypes here*/
+int logIn() {
+
+}
+
+int isAdmin (int ID) {
+        if (ID[1] = 'A') {
+        return 1;
+    }
+}
+
+void menu(); {
+    printf("1. ")
+}
+
+void search();
+
+void edit();
+
+void print();
+
+void add ();
+
+void 
+
+
+
+
+
+/*Main boi big man fuction gonna knock it out of the park*/
+int main (void)
+{
+
+    return 0;
 }
